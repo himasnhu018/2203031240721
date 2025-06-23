2c939a97054eec947cf07c1d5686763152ba956c
himasnhu018 <himanshukrdev007@gmail.com>
2025-06-23 12:10:04 +0530

    basic setup

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+app.use(express.json());
+
+
+
+
+
+app.listen(port, () => {  console.log(`Server is running on http://localhost:${port}`);
+});
